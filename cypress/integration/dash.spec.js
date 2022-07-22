6fc28af074b184d10a3fc4330d35d0a104d09579
Raphael Carvalho <raphilskerj@gmail.com>
2022-07-21 21:34:45 -0300

    Implementação do Command 'apiLogin', cujo o mesmo faz um request para o endpoint '/sessions', para, em seguida, armazenar em variável de ambiente, o token de autenticação gerado pela simulação do login com o endpoint '/sesseions'

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -20,12 +20,32 @@ describe('dashboard', () => {
         }
 
         before(() => {
-            cy.postUser(data.customer),
+            cy.postUser(data.customer)
             cy.postUser(data.samurai)
+
+            cy.apiLogin(data.customer)
+            cy.log(`Consegui o token ${Cypress.env('apiToken')}`)
         })
 
         it('o mesmo deve ser exibido no dashboard', () => {
             console.log(data)
         })
+    })
+})
+
+Cypress.Commands.add('apiLogin', (user) => {
+
+    const payload = {
+        email: user.email,
+        password: user.password
+    }
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/sessions',
+        body: payload
+    }).then((response) => {
+        expect(response.status).to.eq(200)
+        Cypress.env('apiToken', response.body.token)
     })
 })
