c31f7328945aed74900146ea98d194faa4c91271
Raphael Carvalho <raphilskerj@gmail.com>
2022-07-26 18:30:50 -0300

    Implementação de função para habilitar screenshot para todas as execuções e não apenas para os testes que falharem.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,10 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+
+// Screenshots para todos os testes (passando ou falhando)
+/* afterEach(() => {
+    cy.wait(1500)
+    cy.screenshot()
+}) */
