b67460d27ae45d838a983237f09939d43c171eec
Raphael Carvalho <raphilskerj@gmail.com>
2022-07-27 18:20:10 -0300

    Implementação de checkpoints em cada pageload, para garantir que o script está percorrendo o caminho certo

--- cypress/support/pages/resetpass/elements.js
+++ cypress/support/pages/resetpass/elements.js
@@ -1,4 +1,5 @@
 exports.el = {
+    title: ('h1', 'Resetar senha'),
     newPass: 'input[placeholder = "Nova senha"]',
     confirmPass: 'input[placeholder = "Confirmação da senha"]',
     changePassButton: ('button[type=submit]', 'Alterar senha')
